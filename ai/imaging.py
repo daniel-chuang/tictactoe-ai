b3d82e21d41e74b0b592ee1bc172943c072764a8
Daniel Chuang <sharpincludecode@gmail.com>
2021-04-08 22:21:32 -0400

    Changed imaging.py to use pynput. It takes a screenshot of the top corner of the screen (where the game screen is) whenever SPACE is pressed, and ends when the Esc key is pressed. main.py runs both processes simultaneously.

--- ai/imaging.py
+++ ai/imaging.py
@@ -2,8 +2,30 @@
 import os
 import pyautogui
 import time
+import keyboard
+from pynput import keyboard
 
-time.sleep(1)
+#time.sleep(2)
 
-im = pyautogui.screenshot(region=(0,53,600,600))
-im.save("./screen.png")
+def on_press(key):
+        try:
+            pass
+        except AttributeError:
+            pass
+
+
+def on_release(key):
+    if key == keyboard.Key.space:
+        im = pyautogui.screenshot(region=(0,53,600,600))
+        im.save("./screen.png") 
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+while True:
+    pass
